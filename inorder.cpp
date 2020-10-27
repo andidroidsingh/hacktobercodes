762956c3e1443f7a8a921ac6caaadb258b866854
andidroidsingh <73545709+andidroidsingh@users.noreply.github.com>
2020-10-27 23:34:40 +0530

    Add files via upload

---
+++ inorder.cpp
@@ -0,0 +1,53 @@
+#include<bits/stdc++.h>
+using namespace std;
+struct node
+{
+	int data;
+	struct Node *left;
+	struct Node *right;
+	Node(int data)
+	{
+		this->value = data;
+		this->left = this->right = NULL;
+	}
+};
+
+
+Node *makeTree(Node *root, int arr[], int i, int n)
+{
+	if(i<n)
+	{
+		Node *temp = new Node(arr[i]);
+		temp = makeTree(temp, arr, 2*i+0, n);
+		temp= makeTree(temp, arr, 2*i+1, n);
+		root = temp;
+	}
+	return root;
+}
+void inorder(Node *root)
+{
+	if(root==NULL)
+		return;
+	inorder(root->left);
+	cout<<root->data<<" ";
+	inorder(root->right);
+}
+int main()
+{
+	int n;
+	cout<<"Enter the number of elements\n";
+	cin>>n;
+
+	Node root;
+
+	cout<<"Enter  the numbers\n";
+	int arr[n];
+	for(int i=0;i<n;i++)
+	{
+		cin>>arr[i];
+	}
+
+	root = makeTree(root, arr, 0, n);
+	inorder(root);
+	return 0;
+}
